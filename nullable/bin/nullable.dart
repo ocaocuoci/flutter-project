1eddb1d4dfe754c788b5a019fbce962d0aa83dab
oca <mukhammadanshori11@gmail.com>
2026-03-02 07:20:15 +0000

    percobaan nullable

--- nullable/bin/nullable.dart
+++ nullable/bin/nullable.dart
@@ -1,5 +1,9 @@
 import 'package:nullable/nullable.dart' as nullable;
 
 void main(List<String> arguments) {
-  print('Hello world: ${nullable.calculate()}!');
+  String? name;
+  name = null;
+  print(name);
+
+  
 }
