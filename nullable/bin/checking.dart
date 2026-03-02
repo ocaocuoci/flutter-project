2fabe7c30d2545f8de3e2b08ecf3e6e3f5d16501
oca <mukhammadanshori11@gmail.com>
2026-03-02 07:35:23 +0000

    percobaan pengecekan

---
+++ nullable/bin/checking.dart
@@ -0,0 +1,17 @@
+import 'package:nullable/nullable.dart' as nullable;
+import 'dart:io';
+void main(List<String> arguments){
+
+int a = 10;
+int b = 0;
+
+print(a==b);
+print(identical(a, b));
+
+
+//String nama1 = "Oca";
+//String nama2 = "Oca";percobaan 4 ganti huruf kecil
+//print(nama1 == nama2);
+//print(identical(nama1, nama2)); percobaan3
+
+}
