2335735ed1697ccc200c5c4d84eabd103053a520
oca <mukhammadanshori11@gmail.com>
2026-03-02 03:58:18 +0000

    percobaan awal1

--- prak3sib2/bin/prak3sib2.dart
+++ prak3sib2/bin/prak3sib2.dart
@@ -4,6 +4,9 @@ const String name = "Hernanda";
 const int age = 21;
 
 void main(List<String> arguments) {
-  const String name = "Oca";
+  // const String name = "Oca";
+  final String nama = "Hernanda Rizka";
+  final int umur = 20;
   print('Nama saya $name, umur saya $age tahun');
+  print('Nama saya $nama, umur saya $umur tahun');
 }
