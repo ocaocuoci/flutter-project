d41d616fde03ae6abcc62af8128a81a5af151bad
oca <mukhammadanshori11@gmail.com>
2026-03-02 03:59:34 +0000

    menghapus beberapa files

--- praktikdart1/bin/praktikdart1.dart
+++
@@ -1,5 +0,0 @@
-import 'package:praktikdart1/praktikdart1.dart' as praktikdart1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${praktikdart1.calculate()}!');
-}
--- praktikdart1/test/praktikdart1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:praktikdart1/praktikdart1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
