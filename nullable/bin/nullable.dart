10a534a728782d027c26891f4780a734b2e9b725
oca <mukhammadanshori11@gmail.com>
2026-03-02 07:26:41 +0000

    percobaan nullable234

--- nullable/bin/nullable.dart
+++ nullable/bin/nullable.dart
@@ -1,9 +1,21 @@
 import 'package:nullable/nullable.dart' as nullable;
-
+import 'dart:io';
 void main(List<String> arguments) {
-  String? name;
-  name = null;
-  print(name);
+ // String? name;
+ // name = null;
+ // print(name);
 
-  
+  //int? a;
+//a = null;
+//print(a);
+
+//String? nama;
+//nama = 'hernanda';
+//print("Nama saya ${nama ?? 'Tidak diketahui'}"); 
+
+print("Masukkan nama anda : ");
+String? nama = stdin.readLineSync();
+//print("Nama anda adalah : ${nama ?? 'Tidak diketahui'}"); 
+print("Nama anda adalah : ${nama == null || nama.isEmpty ? 'Tidak diketahui' : nama}");
+
 }
